c2dc916b6a1c45670d65eabe4bae48790033e341
Joe Strong <joestrong@gmail.com>
2016-03-10 14:05:00 +0000

    Added server icons

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -21,9 +21,14 @@ class Navigation {
       for (let i = 0; i < config.machines.length; i++) {
         let machine = config.machines[i];
         let el = document.createElement('div');
+        let icon = document.createElement('i');
+        let nameEl = document.createElement('span');
+        icon.className = 'fa fa-server';
+        nameEl.textContent = machine.name;
         el.className = "item";
         el.setAttribute('data-id', i);
-        el.textContent = machine.name;
+        el.appendChild(icon);
+        el.appendChild(nameEl)
         this.shadowRoot.appendChild(el);
       }
     });
@@ -31,8 +36,9 @@ class Navigation {
 
   bindEvents() {
     this.shadowRoot.addEventListener('click', (e) => {
-      e.target.className += ' active';
-      this.onSelect(config.machines[e.target.getAttribute('data-id')]);
+      let currentTarget = (e.target.classList.contains('item')) ? e.target : e.target.parentElement;
+      currentTarget.className += ' active';
+      this.onSelect(config.machines[currentTarget.getAttribute('data-id')]);
     });
   }
 }
